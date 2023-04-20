6307706944e55b66d89b453fff24835a026fae4e
cxk_git <1720921998@qq.com>
2023-04-20 14:34:06 +0800

    项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,9 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="container">
+    {{ $store.state.user.profile }}
+    {{ $store.state.user.profile.account }}
+    <button @click="$store.commit('user/setUser', { account: 'zhangsan' })">修改用户信息</button>
+  </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<style lang="less"></style>
