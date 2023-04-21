65e13bd9c1019bef604711082b671971f074d979
cxk_git <1720921998@qq.com>
2023-04-21 23:39:24 +0800

    完成头部，底部

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,17 +1,19 @@
 <template>
   <AppTopnav>顶部组件</AppTopnav>
-  <header>头部组件</header>
+  <AppHeader>头部组件</AppHeader>
   <div class="main">
     <router-view></router-view>
   </div>
-  <footer>底部组件</footer>
+  <AppFooter>底部组件</AppFooter>
 </template>
 
 <script>
 import AppTopnav from '@/components/app-topnav'
+import AppHeader from '@/components/app-header.vue'
+import AppFooter from '@/components/app-footer.vue'
 export default {
   name: 'Layout',
-  components: { AppTopnav }
+  components: { AppTopnav, AppHeader, AppFooter }
 }
 </script>
 
