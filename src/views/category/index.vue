bda0e6b8f65e23eb843b4a8e64bb28dacfc622f9
cxk_git <1720921998@qq.com>
2023-04-28 18:15:13 +0800

    面包屑初级—使用基本的封装

--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="container">
-    Top-Category
+  <div class="top-category">
+    <div class="container">
+      <XtxBread parentPath="/category/1005000" parentName="电器">空调</XtxBread>
+      <!-- 面包屑 -->
+      <!-- 轮播图 -->
+      <!-- 全部分类 -->
+      <!-- 各个分类下的推荐商品 -->
+    </div>
   </div>
 </template>
 
