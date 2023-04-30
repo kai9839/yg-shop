ecb631ea01baa6dab142b763bd78be6126587b41
cxk_git <1720921998@qq.com>
2023-04-30 13:52:08 +0800

    二级类目面包屑

--- src/views/category/sub.vue
+++ src/views/category/sub.vue
@@ -1,12 +1,17 @@
 <template>
-  <div class="container">
-    Sub-Category
+  <div class='sub-category'>
+    <div class="container">
+      <!-- 面包屑 -->
+      <SubBread />
+    </div>
   </div>
 </template>
 
 <script>
+import SubBread from './components/sub-bread'
 export default {
-  name: 'SubCategory'
+  name: 'SubCategory',
+  components: { SubBread }
 }
 
 </script>
