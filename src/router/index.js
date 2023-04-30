5388efde8d5957d8f65ff5834001e0b277907684
cxk_git <1720921998@qq.com>
2023-04-30 13:08:12 +0800

    切换路由自动回到顶部

--- src/router/index.js
+++ src/router/index.js
@@ -22,7 +22,12 @@ const routes = [
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
+  routes,
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else return { left: 0, top: 0 }
+  }
 })
 
 export default router
