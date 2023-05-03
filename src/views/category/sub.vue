e537a67841f831d3cc87b95d0e26a767f695cedd
cxk_git <1720921998@qq.com>
2023-05-03 23:37:09 +0800

    数据加载

--- src/views/category/sub.vue
+++ src/views/category/sub.vue
@@ -10,6 +10,13 @@
         <!-- 排序 -->
         <SubSort />
         <!-- 列表 -->
+        <ul>
+          <li v-for="goods in goodsList" :key="goods.id" >
+            <GoodsItem :goods="goods" />
+          </li>
+        </ul>
+        <!-- 加载 -->
+        <XtxInfiniteLoading :loading="loading" :finished="finished" @infinite="getData" />
       </div>
     </div>
   </div>
@@ -19,16 +26,79 @@
 import SubBread from './components/sub-bread'
 import SubFilter from './components/sub-filter'
 import SubSort from './components/sub-sort'
+import GoodsItem from './components/goods-itme'
+import { ref, watch } from 'vue'
+import { findSubCategoryGoods } from '@/api/category'
+import { useRoute } from 'vue-router'
 export default {
   name: 'SubCategory',
-  components: { SubBread, SubFilter, SubSort }
-}
+  components: { SubBread, SubFilter, SubSort, GoodsItem },
+  setup () {
+    // 1. 基础布局
+    // 2. 无限加载组件
+    // 3. 动态加载数据且渲染
+    // 4. 任何筛选条件变化需要更新列表
+    const route = useRoute()
 
+    const loading = ref(false)
+    const finished = ref(false)
+    const goodsList = ref([])
+    // 查询参数
+    let reqParams = {
+      page: 1,
+      pageSize: 20
+    }
+    // 获取数据函数
+    const getData = () => {
+      loading.value = true
+      // 设置二级分类的ID
+      reqParams.categoryId = route.params.id
+      findSubCategoryGoods(reqParams).then(({ result }) => {
+        // 获取数据成功
+        if (result.items.length) {
+          // 有数据就追加数据
+          goodsList.value.push(...result.items)
+          // 把page改成下一页页码
+          reqParams.page++
+        } else {
+          // 没有数据，代表加载完成
+          finished.value = true
+        }
+        loading.value = false
+      })
+    }
+
+    // 在更改了二级分类的时候需要重新加载数据
+    watch(() => route.params.id, (newVal) => {
+      if (newVal && `/category/sub/${newVal}` === route.path) {
+        finished.value = false
+        goodsList.value = [] // 导致列表空的，加载更多组件顶上来，进入可视区，区加载数据
+        reqParams = {
+          page: 1,
+          pageSize: 20
+        }
+      }
+    })
+    return { loading, finished, goodsList, getData }
+  }
+}
 </script>
 <style  lang="less" scoped>
 .goods-list {
   background: #fff;
   padding: 0 25px;
   margin-top: 25px;
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 0 5px;
+    li {
+      margin-right: 20px;
+      margin-bottom: 20px;
+      &:nth-child(5n) {
+        margin-right: 0;
+      }
+    }
+  }
 }
 </style>
