5b53666c69f46fb27fcf091f0bccb7bea4b75277
cxk_git <1720921998@qq.com>
2023-05-03 22:38:00 +0800

    排序组件

--- src/views/category/sub.vue
+++ src/views/category/sub.vue
@@ -5,6 +5,12 @@
       <SubBread />
       <!-- 筛选区 -->
       <SubFilter />
+      <!-- 结果区域 -->
+      <div class="goods-list">
+        <!-- 排序 -->
+        <SubSort />
+        <!-- 列表 -->
+      </div>
     </div>
   </div>
 </template>
@@ -12,11 +18,17 @@
 <script>
 import SubBread from './components/sub-bread'
 import SubFilter from './components/sub-filter'
+import SubSort from './components/sub-sort'
 export default {
   name: 'SubCategory',
-  components: { SubBread, SubFilter }
+  components: { SubBread, SubFilter, SubSort }
 }
 
 </script>
 <style  lang="less" scoped>
+.goods-list {
+  background: #fff;
+  padding: 0 25px;
+  margin-top: 25px;
+}
 </style>
