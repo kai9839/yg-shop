fed3f04c250954a95447eec3e0beddd432e7e4ef
cxk_git <1720921998@qq.com>
2023-05-08 22:23:03 +0800

    图片放大镜02

--- src/views/goods/components/goods-images.vue
+++ src/views/goods/components/goods-images.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="goods-image">
-    <div class="large" :style="[{backgroundImage:`url(${images[currIndex]})`},bgPosition]"></div>
+    <!-- 大图 -->
+    <div v-show="show" class="large" :style="[{backgroundImage:`url(${images[currIndex]})`},largePosition]"></div>
+    <!-- 中图 -->
     <div class="middle" ref="target">
       <img :src="images[currIndex]" alt="">
-      <div class="layer" v-show="show" :style="position"></div>
+      <!-- 遮罩色块 -->
+      <div class="layer" v-show="show" :style="layerPosition"></div>
     </div>
+    <!-- 小图 -->
     <ul class="small">
       <li v-for="(img,i) in images" :key="img" :class="{active:i===currIndex}">
         <img @mouseenter="currIndex=i" :src="img" alt="">
@@ -24,38 +28,45 @@ export default {
     }
   },
   setup (props) {
+    // 当前预览图的索引
     const currIndex = ref(0)
-    const usePreviewImg = () => {
-      const target = ref(null)
-      const show = ref(false)
-      // elementX 鼠标基于容器左上角X轴偏移
-      // elementY 鼠标基于容器左上角Y轴偏移
-      // isOutside 鼠标是否在模板容器外
-      const { elementX, elementY, isOutside } = useMouseInElement
-      const position = reactive({ left: 0, top: 0 })
-      const bgPosition = reactive({ backgroundPositionX: 0, backgroundPositionY: 0 })
-      watch([elementX, elementY, isOutside], () => {
-        // 控制X轴方向的定位 0-200 之间
-        if (elementX.value < 100) position.left = 0
-        else if (elementX.value > 300) position.left = 200
-        else position.left = elementX.value - 100
-        // 控制Y轴方向的定位 0-200 之间
-        if (elementY.value < 100) position.top = 0
-        else if (elementY.value > 300) position.top = 200
-        else position.top = elementY.value - 100
-        // 设置大背景的定位
-        bgPosition.backgroundPositionX = -position.left * 2 + 'px'
-        bgPosition.backgroundPositionY = -position.top * 2 + 'px'
-        // 设置遮罩容器的定位
-        position.left = position.left + 'px'
-        position.top = position.top + 'px'
-        // 设置是否显示预览大图
-        show.value = !isOutside.value
-      })
-      return { position, bgPosition, show, target }
-    }
-    const { position, bgPosition, show, target } = usePreviewImg()
-    return { currIndex, position, bgPosition, show, target }
+    // 1. 是否显示遮罩和大图
+    const show = ref(false)
+    // 2. 遮罩的坐标(样式)
+    const layerPosition = reactive({
+      left: 0,
+      top: 0
+    })
+    // 3. 大图背景定位(样式)
+    const largePosition = reactive({
+      backgroundPositionX: 0,
+      backgroundPositionY: 0
+    })
+    // 4. 使用useMouseInElement得到基于元素左上角的坐标和是否离开元素数据
+    // elementX 鼠标基于容器左上角X轴偏移
+    // elementY 鼠标基于容器左上角Y轴偏移
+    // isOutside 鼠标是否在模板容器外
+    const target = ref(null)
+    const { elementX, elementY, isOutside } = useMouseInElement(target)
+    watch([elementX, elementY, isOutside], () => {
+      // 5. 根据得到数据设置样式数据和是否显示数据
+      show.value = !isOutside.value
+      // 计算坐标
+      const position = { x: 0, y: 0 }
+      if (elementX.value < 100) position.x = 0
+      else if (elementX.value > 300) position.x = 200
+      else position.x = elementX.value - 100
+
+      if (elementY.value < 100) position.y = 0
+      else if (elementY.value > 300) position.y = 200
+      else position.y = elementY.value - 100
+      // 给样式赋值
+      layerPosition.left = position.x + 'px'
+      layerPosition.top = position.y + 'px'
+      largePosition.backgroundPositionX = -2 * position.x + 'px'
+      largePosition.backgroundPositionY = -2 * position.y + 'px'
+    })
+    return { currIndex, show, layerPosition, largePosition, target }
   }
 }
 </script>
