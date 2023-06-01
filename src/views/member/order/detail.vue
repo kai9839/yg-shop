37e564b24e811039a64b4fd909fa8a754f0df4ea
cxk_git <1720921998@qq.com>
2023-06-01 21:24:30 +0800

    订单详情-头部展示

--- src/views/member/order/detail.vue
+++ src/views/member/order/detail.vue
@@ -1,14 +1,32 @@
 <template>
-  <div class="member-order-detail">
-    orderDetail
+  <div class="order-detail-page" v-if="order">
+    <!-- 操作栏 -->
+    <DetailAction :order="order" />
+    <!-- 步骤条 组件xtx-steps.vue-->
+    <!-- 物流栏 -->
+    <!-- 订单商品信息 -->
   </div>
 </template>
-
 <script>
+import { findOrder } from '@/api/order'
+import { useRoute } from 'vue-router'
+import DetailAction from './components/detail-action'
+import { ref } from 'vue'
 export default {
-  name: 'orderDetail'
+  name: 'OrderDetailPage',
+  components: { DetailAction },
+  setup () {
+    const order = ref(null)
+    const route = useRoute()
+    findOrder(route.params.id).then(data => {
+      order.value = data.result
+    })
+    return { order }
+  }
 }
-
 </script>
-<style  lang="less" scoped>
+<style scoped lang="less">
+.order-detail-page {
+  background: #fff;
+}
 </style>
